e1ac948cd2bfc513064f952a7d0cd3669be5137b
Leopold <czy.leopold97@gmail.com>
2025-08-29 02:25:57 +1000

    update routes with mongoDB connection

--- user-service/src/routes/userRoutes.ts
+++ user-service/src/routes/userRoutes.ts
@@ -1,24 +1,53 @@
-import e, { Router } from "express";
+import { Request, Response, Router } from "express";
+import User, { IUser } from "../models/User";
 
 const router = Router();
 
-//get /api/users/123
+// api/users
+router.get("/", async (req:Request, res:Response) => {
+    try {
+        const users:IUser[] = await User.find();
+        res.status(201).json({users});
+    } catch (err: any) {
+        res.status(500).json({error: err.message});
+    }
+});
+
+
+// api/users/123
 router.get("/:id", (req, res) => {
     const {id} = req.params;
     res.json({message: `user with id ${id}:`})
 });
 
 
-//get /api/users/search?name=tom
-router.get("/search", (req,res) => {
+// api/users/search?name=tom
+router.get("/search", async (req:Request,res:Response) => {
     const {name} = req.query;
-    res.json({message: `Your are searching for ${name}`})
+    if (!name) {
+        return res.status(400).json({error: "no name is contained in your search!"})
+    }
+
+    try {
+        const user = await User.find({username: name});
+        res.status(201).json(user);
+    } catch (err: any) {
+        res.status(500).json({error: err.message});
+    }
 })
 
-router.post("/register", async (req, res) => {
+
+// register
+router.post("/register", async (req: Request, res:Response) => {
     try {        
         const {username, email, password} = req.body;
-        res.status(201).json({username, email, password});
+        if (!(username&&email&&password)) {
+            return res.status(400).json({error: "Missing required fields!"})
+        }
+
+        const newUser = new User({username, email, password});
+        await newUser.save();
+        res.status(201).json(newUser);
     } catch (error: any) {
         res.status(500).json({error: error.message});
     }
