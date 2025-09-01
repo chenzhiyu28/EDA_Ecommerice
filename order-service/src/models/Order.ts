17437ddd4109c4bbd68c870e42949a81ed633b48
Leopold <czy.leopold97@gmail.com>
2025-09-01 11:05:02 +1000

    complete order model

---
+++ order-service/src/models/Order.ts
@@ -0,0 +1,22 @@
+import mongoose, { Document, Schema } from "mongoose";
+
+export interface IOrder extends Document {
+    user: string;
+    amount: number;
+    status: string;
+};
+
+const OrderSchema: Schema = new Schema(
+    {
+        user: { type: mongoose.Schema.Types.ObjectId,
+            ref: "User",
+            required: true
+        },
+        amount: {type: Number, required: true},
+        status: {type: String, required: true},
+    },
+    {timestamps: true},
+);
+
+const orderModel = mongoose.model<IOrder>("Order", OrderSchema);
+export default orderModel;
