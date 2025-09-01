67419e64d541e8b014c88ec1b37c559e811c936d
Leopold <czy.leopold97@gmail.com>
2025-09-02 00:17:44 +1000

    complete order routes

--- order-service/src/routes/orderRoutes.ts
+++ order-service/src/routes/orderRoutes.ts
@@ -1,17 +1,34 @@
-import { Request, Response, Router } from "express";
+import { Router } from "express";
+import orderModel from "../models/Order";
 
 const router = Router();
 
 
 // api/orders
-router.get("/orders", (req, res) => {
+router.get("/orders", async (req, res) => {
     try {
-        res.status(200).send("you are visiting all orders!")
+        const orders = await orderModel.find();
+        res.status(201).json(orders);
     } catch (err: any) {
         res.status(500).json({error: err.message})
     }
 })
 
+
+// api/order
+router.post("/order", async (req, res) => {
+    const {userID, amount, status} = req.body;
+    if (!(userID&&amount&&status)) {
+        return res.status(400).json({err: "Missing required fields!"});
+    }
+    try {
+        const order = new orderModel(userID, amount, status);
+        await order.save();
+        res.status(201).json(order);
+    } catch(err: any) {
+        res.status(500).json({error: err.message});
+    }
+})
 
 
 export default router;
--- user-service/src/routes/userRoutes.ts
+++ user-service/src/routes/userRoutes.ts
@@ -1,12 +1,12 @@
 import { Request, Response, Router } from "express";
-import User, { IUser } from "../models/User";
+import userModel, { IUser } from "../models/User";
 
 const router = Router();
 
 // api/users
 router.get("/", async (req:Request, res:Response) => {
     try {
-        const users:IUser[] = await User.find();
+        const users:IUser[] = await userModel.find();
         res.status(201).json({users});
     } catch (err: any) {
         res.status(500).json({error: err.message});
@@ -19,7 +19,7 @@ router.get("/:name", async (req, res) => {
     const {name} = req.params;
     
     try {
-        const user = await User.find({username:`${name}`});
+        const user = await userModel.find({username:`${name}`});
         res.status(200).json(user);
     } catch(err: any) {
         res.status(500).json({error: err.message});
@@ -35,7 +35,7 @@ router.get("/search", async (req:Request,res:Response) => {
     }
 
     try {
-        const user = await User.find({username: name});
+        const user = await userModel.find({username: name});
         res.status(201).json(user);
     } catch (err: any) {
         res.status(500).json({error: err.message});
@@ -51,7 +51,7 @@ router.post("/register", async (req: Request, res:Response) => {
             return res.status(400).json({error: "Missing required fields!"})
         }
 
-        const newUser = new User({username, email, password});
+        const newUser = new userModel({username, email, password});
         await newUser.save();
         res.status(201).json(newUser);
     } catch (error: any) {
